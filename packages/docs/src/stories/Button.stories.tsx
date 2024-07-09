fd9cd6e156f912a3948a95aae06dabb1bb171278
João Vinícius Guerber de Souza <joaoviniciusgs@gmail.com>
2024-07-09 12:54:30 -0300

    implementado componentes

---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,51 @@
+
+import { Button, ButtonProps } from "@guerber-ui/react";
+import type { StoryObj, Meta } from "@storybook/react";
+import { ArrowRight } from "phosphor-react";
+
+export default {
+    title: "Form/Button",
+    component: Button,
+    args: {
+        children: "Send"
+    },
+} as Meta<ButtonProps>
+
+
+
+export const Primary: StoryObj<ButtonProps> = {}
+export const Disabled: StoryObj<ButtonProps> = {
+    args: {
+        disabled: true,
+    }
+}
+
+export const Secondary: StoryObj<ButtonProps> = {
+    args: {
+        variant: 'secondary'
+    }
+}
+export const Tertiary: StoryObj<ButtonProps> = {
+    args: {
+        variant: 'tertiary',
+        children: "Cancel"
+    }
+}
+
+export const WithIcon: StoryObj<ButtonProps> = {
+    args: {
+        children: (
+            <>
+                Próximo passo
+                <ArrowRight weight="bold" />
+            </>
+        )
+    }
+}
+
+export const Small: StoryObj<ButtonProps> = {
+    args: {
+        size: 'sm',
+    }
+}
+
